bf5deb5e08e07472059a607b9a5e2d71c3f2872b
zzidante <zzidante@gmail.com>
2020-05-17 23:38:07 -0700

    Fix migration table existance behaviour so it returns a boolean

--- server/db/migrator/sql/check_schema_migration_table_exists.sql
+++ server/db/migrator/sql/check_schema_migration_table_exists.sql
@@ -1,3 +1,15 @@
+-- as `to_regclass` returns the string NULL if there's no match, we need to do this 
+-- crazy excess stuff to essentially perform a `does this name exist in the regclass?`
+--
+
+WITH reg_class_normalized AS (
+  SELECT
+    NULLIF(
+      to_regclass('public.schema_migrations')::TEXT,
+	    'NULL'
+    ) AS exists
+) 
+            
 SELECT EXISTS(
-  SELECT to_regclass('public.migrations')
+  SELECT 1 FROM reg_class_normalized WHERE exists IS NOT NULL
 );
